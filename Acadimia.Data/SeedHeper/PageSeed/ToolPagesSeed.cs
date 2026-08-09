34889069d0f805ce0fdd9af836772bbf00ac4a71
Ziad Kamal Al-Numailat <124497442+ziad2001kamal@users.noreply.github.com>
2026-08-09 15:16:30 +0300

    add seed

--- Acadimia.Data/SeedHeper/PageSeed/ToolPagesSeed.cs
+++ Acadimia.Data/SeedHeper/PageSeed/ToolPagesSeed.cs
@@ -1,1 +1,383 @@
-﻿
+﻿using Acadimia.Data.Enums;
+using Acadimia.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acadimia.Data.SeedHeper.PageSeed
+{
+    public static class ToolPagesSeed
+    {
+        public static List<Page> AddToollPages(int lastPageId)
+        {
+            var toolPages = new List<Page>()
+            {
+                new Page()
+                {
+                    Name = "عرض بيانات جدول المستخدمين",
+                    NameEn = "Display User DataTable",
+                    Icon = null,
+                    Link ="User/GetAll",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.UserPageId, // User Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "اظهار واجهة اضافة  تعديل مستخدم",
+                    NameEn = "Display Create Edit User Page",
+                    Icon = null,
+                    Link ="User/CreateEditModal",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.UserPageId, // User Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "اضافة تعديل مستخدم",
+                    NameEn = "Create Edit User",
+                    Icon = null,
+                    Link ="User/CreateEdit",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.UserPageId, // User Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "حذف مستخدم",
+                    NameEn = "Delete User",
+                    Icon = null,
+                    Link ="User/Delete",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.UserPageId, // User Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "عرض واجهة ملفي الشخصي",
+                    NameEn = "Display My Profile Page",
+                    Icon = null,
+                    Link ="User/MyProfileModal",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.UserPageId, // User Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "تعديل ملفي الشخصي",
+                    NameEn = "Update My Profile",
+                    Icon = null,
+                    Link ="User/MyProfile",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.UserPageId, // User Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "عرض واجهة تغير كلمة المرور",
+                    NameEn = "Display Change Password Page",
+                    Icon = null,
+                    Link ="User/ChangePasswordModal",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.UserPageId, // User Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "تغير كلمة المرور",
+                    NameEn = "ChangePassword",
+                    Icon = null,
+                    Link ="User/ChangePassword",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.UserPageId, // User Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "عرض بيانات جدول انواع المستخدين",
+                    NameEn = "Display User Type DateTable",
+                    Icon = null,
+                    Link ="UserType/GetAll",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.UserTypePageId, // UserType Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "عرض واجهة اضافة  تعديل نوع المستخدم",
+                    NameEn = "Display Create Edit User Type page",
+                    Icon = null,
+                    Link ="UserType/CreateEditModal",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.UserTypePageId, // UserType Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "اضافة تعديل نوع مستخدم",
+                    NameEn = "Create Edit User Type ",
+                    Icon = null,
+                    Link ="UserType/CreateEdit",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.UserTypePageId, // UserType Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "حذف نوع مستخدم",
+                    NameEn =  "Delete User Type ",
+                    Icon = null,
+                    Link ="UserType/Delete",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.UserTypePageId, // UserType Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "عرض صلاحيات نوع المستخدم",
+                    NameEn =  "display User Type Permissions",
+                    Icon = null,
+                    Link ="UserPermission/GetUserTypePermissions",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.UserPermissionsId, // User Permissions Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "حفظ صلاحيات نوع المستخدم",
+                    NameEn =  "Save User Type Permissions",
+                    Icon = null,
+                    Link ="UserPermission/SavePermissions",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.UserPermissionsId, // User Permissions Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "عرض بيانات جدول المحافظات والمدن",
+                    NameEn =  "Display Governorates and Cities DateTable",
+                    Icon = null,
+                    Link ="Destination/GetAll",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.DestinationId, // Destinations Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "عرض واجهة إضافة تعديل وجهة",
+                    NameEn =  "Display create Edit Destination page",
+                    Icon = null,
+                    Link ="Destination/CreateEditModal",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.DestinationId, // Destinations Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "إضافة تعديل وجهة",
+                    NameEn =  "create Edit Destination",
+                    Icon = null,
+                    Link ="Destination/CreateEdit",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.DestinationId, // Destinations Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "حذف وجهة",
+                    NameEn =  "Delete Destination",
+                    Icon = null,
+                    Link ="Destination/Delete",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.DestinationId, // Destinations Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "تبديل حالات وحدات النظام",
+                    NameEn =  "Switching states of system Modules",
+                    Icon = null,
+                    Link ="Management/SwitchStatus",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.SystemModulesId, // System Modules Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "عرض بيانات جدول الصفحات",
+                    NameEn =  "Display Pages DataTable",
+                    Icon = null,
+                    Link ="Page/GetAll",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.PageId, // System Modules Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "عرض واجهة إضافة  تعديل صفحة",
+                    NameEn =  "Display Create Edit Page interface",
+                    Icon = null,
+                    Link ="Page/CreateEditModal",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.PageId, // System Modules Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "إضافة تعديل صفحة",
+                    NameEn =  "Create Edit Page",
+                    Icon = null,
+                    Link ="Page/CreateEdit",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.PageId, // System Modules Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "حذف صفحة",
+                    NameEn =  "Delete Page",
+                    Icon = null,
+                    Link ="Page/Delete",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.PageId, // System Modules Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "عرض بيانات جدول الثوابت",
+                    NameEn =  "Display Constant DataTable",
+                    Icon = null,
+                    Link ="Constant/GetAll",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.ConstantId, // Constant Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "عرض واجهة إضافة تعديل ثوابت",
+                    NameEn =  "Display Create Edit Constant Page",
+                    Icon = null,
+                    Link ="Constant/CreateEditModal",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.ConstantId, // Constant Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "إضافة تعديل ثوابت",
+                    NameEn =  "Create Edit Constant",
+                    Icon = null,
+                    Link ="Constant/CreateEdit",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.ConstantId, // Constant Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+                new Page()
+                {
+                    Name = "حذف ثابت",
+                    NameEn =  "Delete Constant",
+                    Icon = null,
+                    Link ="Constant/Delete",
+                    InMenu = false,
+                    IsAjax = true,
+                    ParentId = (int)GeneralEnums.ConstantId, // Constant Page
+                    IsActive = true,
+                    ModuleId = (int)GeneralEnums.Management,
+                    CategoryId = (int)GeneralEnums.Tool
+                },
+
+                  };
+
+
+            lastPageId++;
+            foreach (var page in toolPages)
+            {
+                page.Id = lastPageId++;
+            }
+
+            return toolPages;
+        }
+    }
+}
--- Acadimia.Data/SeedHeper/SeedHelper.cs
+++ Acadimia.Data/SeedHeper/SeedHelper.cs
@@ -1,1 +1,161 @@
-﻿
+﻿using Acadimia.Data.Enums;
+using Acadimia.Data.Models;
+using Acadimia.Data.SeedHeper;
+using Acadimia.Data.SeedHeper.PageSeed;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fast.Data.SeedHeper
+{
+    public static class SeedHelper
+    {
+        public static void Seed(this ModelBuilder builder)
+        {
+            SeedPageCategories(builder);
+            SeedModules(builder);
+            SeedConstants(builder);
+            PagesSeed.Seed(builder);
+            NationalitySeed.Seed(builder);
+        }
+
+        private static void SeedPageCategories(ModelBuilder builder)
+        {
+            builder.Entity<PageCategory>().HasData(
+                new PageCategory { Id = 1, Name = "Header" },
+                new PageCategory { Id = 2, Name = "Page" },
+                new PageCategory { Id = 3, Name = "Tool" }
+            );
+        }
+
+        private static void SeedModules(ModelBuilder builder)
+        {
+            builder.Entity<Module>().HasData(
+                new Module { Id = 1, Name = "الادارة", Status = true },
+                new Module { Id = 2, Name = "إدارة العملاء", Status = true },
+                new Module { Id = 3, Name = "إدارة الخدمات", Status = true },
+                new Module { Id = 4, Name = "المالية", Status = true },
+                new Module { Id = 5, Name = "البريد", Status = false },
+                new Module { Id = 6, Name = "المصروفات", Status = false },
+                new Module { Id = 7, Name = "الخدمات", Status = false },
+                new Module { Id = 8, Name = "التقارير", Status = false }
+            );
+        }
+
+        private static void SeedConstants(ModelBuilder builder)
+        {
+            builder.Entity<Constant>().HasData(
+                new Constant { Id = 1, Name = "العملة" }, // Currency
+                new Constant { Id = 2, Name = "دولار", ParentId = 1 },
+                new Constant { Id = 3, Name = "دينار", ParentId = 1 },
+                new Constant { Id = 4, Name = "شيكل", ParentId = 1 },
+
+                new Constant { Id = 5, Name = "الجنس" }, // Gender
+                new Constant { Id = 6, Name = "ذكر", ParentId = 5 },
+                new Constant { Id = 7, Name = "أنثى", ParentId = 5 },
+
+                new Constant { Id = 8, Name = "نوع المكان المقصود" }, // DestinationType
+                new Constant { Id = 9, Name = "دولة", ParentId = 8 }, // Country
+                new Constant { Id = 10, Name = "مدينة", ParentId = 8 }, // City
+                new Constant { Id = 11, Name = "محافظة", ParentId = 8 }, // Province
+
+                new Constant { Id = 12, Name = "نوع المرفق" }, // AttachmentType
+                new Constant { Id = 13, Name = "جواز سفر", ParentId = 12 }, // PassportAttachment
+                new Constant { Id = 14, Name = "هوية", ParentId = 12 }, // IdentityAttachment
+                new Constant { Id = 15, Name = "شهادة ثانوية عامة", ParentId = 12 },
+                new Constant { Id = 16, Name = "شهادة دبلوم", ParentId = 12 },
+                new Constant { Id = 17, Name = "شهادة بكالوريس", ParentId = 12 },
+                new Constant { Id = 18, Name = "شهادة ماجستير", ParentId = 12 }
+
+                //new Constant { Id = 20, Name = "نوع المستفيد" }, // BeneficiaryType
+                //new Constant { Id = 21, Name = "طال", ParentId = 20 },
+                //new Constant { Id = 22, Name = "مورد", ParentId = 20 },
+                //new Constant { Id = 23, Name = "مزود خدمة", ParentId = 20 },
+
+                
+                //new Constant { Id = 27, Name = "نوع العنوان" }, // AddressType
+                //new Constant { Id = 28, Name = "عنوان السكن", ParentId = 27 },
+                //new Constant { Id = 29, Name = "عنوان العمل", ParentId = 27 },
+                //new Constant { Id = 30, Name = "العنوان التعليمي", ParentId = 27 },
+                //new Constant { Id = 31, Name = "عنوان مؤقت", ParentId = 27 },
+                //new Constant { Id = 32, Name = "عنوان الشحن", ParentId = 27 },
+
+                //new Constant { Id = 33, Name = "نوع جهة الاتصال" }, // ContactType
+                //new Constant { Id = 34, Name = "رقم موبايل", ParentId = 33 },
+                //new Constant { Id = 35, Name = "رقم هاتف", ParentId = 33 },
+                //new Constant { Id = 36, Name = "رقم واتساب", ParentId = 33 },
+                //new Constant { Id = 37, Name = "رقم تيلجرام", ParentId = 33 },
+
+                //new Constant { Id = 38, Name = "نوع الهوية" }, // IdentityType
+                //new Constant { Id = 39, Name = "بطاقة تعريفية", ParentId = 38 },
+                //new Constant { Id = 40, Name = "هوية مدنية", ParentId = 38 },
+
+                //new Constant { Id = 41, Name = "الديانة" }, // Religion
+                //new Constant { Id = 42, Name = "الإسلام", ParentId = 41 },
+                //new Constant { Id = 43, Name = "المسيحية", ParentId = 41 },
+
+                //new Constant { Id = 44, Name = "صلة القرابة" }, // Kinship
+                //new Constant { Id = 45, Name = "اب", ParentId = 44 },
+                //new Constant { Id = 46, Name = "ام", ParentId = 44 },
+                //new Constant { Id = 47, Name = "ابن", ParentId = 44 },
+                //new Constant { Id = 48, Name = "بنت", ParentId = 44 },
+                //new Constant { Id = 49, Name = "زوج", ParentId = 44 },
+                //new Constant { Id = 50, Name = "زوجة", ParentId = 44 },
+
+                //new Constant { Id = 51, Name = "تصنيف المستفيد" }, // BeneficiaryCategory
+                //new Constant { Id = 52, Name = "موثوق", ParentId = 51 },
+                //new Constant { Id = 53, Name = "غير موثوق", ParentId = 51 },
+                //new Constant { Id = 54, Name = "غير مصنف", ParentId = 51 },
+
+                //new Constant { Id = 55, Name = "مجموعات الخدمات" }, // Service Group
+                //new Constant { Id = 56, Name = "خدمات جوازات السفر", ParentId = 55 },
+                //new Constant { Id = 57, Name = "خدمات الهويات", ParentId = 55 },
+                //new Constant { Id = 58, Name = "خدمات العقود", ParentId = 55 },
+                //new Constant { Id = 59, Name = "خدمات التوثيقات", ParentId = 55 },
+
+                //new Constant { Id = 60, Name = "الأولوية", }, // Priority
+                //new Constant { Id = 61, Name = "إجباري", ParentId = 60 },
+                //new Constant { Id = 62, Name = "إختياري", ParentId = 60 },
+
+                //new Constant { Id = 63, Name = "نوع الحسابات" }, // Accounts Types
+                //new Constant { Id = 64, Name = "مصروفات", ParentId = 63 }, // Expense Accounts
+                //new Constant { Id = 65, Name = "مصروفات", ParentId = 63 }, // Revenue Accounts
+                //new Constant { Id = 66, Name = "ذمم مدينة", ParentId = 63 },
+                //new Constant { Id = 67, Name = "ذمم دائنة", ParentId = 63 },
+                //new Constant { Id = 68, Name = "صناديق", ParentId = 63 }, // Box Accounts
+                //new Constant { Id = 69, Name = "بنوك", ParentId = 63 }, // Bank Accounts
+                //new Constant { Id = 70, Name = "مشتريات", ParentId = 63 },
+                //new Constant { Id = 71, Name = "مبيعات", ParentId = 63 },
+
+                //new Constant { Id = 72, Name = " انواع الجهات" }, // Agency Types
+                //new Constant { Id = 73, Name = "جهات حكومية", ParentId = 72 },
+                //new Constant { Id = 74, Name = "جهات خدماتية", ParentId = 72 },
+                //new Constant { Id = 75, Name = "مندوب شركات", ParentId = 72 },
+                //new Constant { Id = 76, Name = "شركات خاصة", ParentId = 72 },
+
+                //new Constant { Id = 77, Name = " حالات الطلب" }, // Request Cases
+                //new Constant { Id = 78, Name = "جديد", ParentId = 77 },
+                //new Constant { Id = 79, Name = "قيدالتنفيذ", ParentId = 77 },
+                //new Constant { Id = 80, Name = "جاري الإرسال", ParentId = 77 },
+                //new Constant { Id = 81, Name = "جاري الإستلام", ParentId = 77 },
+                //new Constant { Id = 82, Name = "الإستلام من المكتب", ParentId = 77 },
+                //new Constant { Id = 83, Name = "توصيل مع الديلفري", ParentId = 77 },
+                //new Constant { Id = 84, Name = "تم الإستلام", ParentId = 77 },
+                //new Constant { Id = 85, Name = "معلق", ParentId = 77 },
+                //new Constant { Id = 86, Name = "ملغي", ParentId = 77 },
+
+                //new Constant { Id = 87, Name = "حالة الفاتورة" }, // Invoice Status
+                //new Constant { Id = 88, Name = "جديد", ParentId = 87 },
+                //new Constant { Id = 89, Name = "مرحل", ParentId = 87 },
+                //new Constant { Id = 90, Name = "معدل", ParentId = 87 },
+                //new Constant { Id = 91, Name = "لغي", ParentId = 87 }
+
+            );
+        }
+
+     
+    }
+}
